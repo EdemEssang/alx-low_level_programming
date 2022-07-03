0b3669ea4e32dd0e59390f0072dffde3586a563d
Edem <edemoski.eu@gmail.com>
2022-07-03 09:37:56 -0700

    a C program that prints exactly with proper grammar, but the outcome is a piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+
+/**
+ *  Description: A C program that prints with printf function.
+ * */
+
+
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}
