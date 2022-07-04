520683585d9c0b802d1781db9fc26a943751b179
Edem <edemoski.eu@gmail.com>
2022-07-04 10:21:24 -0700

    Numberz

---
+++ 0x01_variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * A program that prints all single digit numbers of base 10 starting from
+ * 0, only using putchar and without char variables
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+
+}
