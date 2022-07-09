6e1938ab954ed9b7212499dfe46057ec28346fad
Edem <edemoski.eu@gmail.com>
2022-07-08 20:51:48 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**This is a function that check if a character is upper case
+ */
+
+int_isupper(int c)
+{
+
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	return (0);
+
+
+
+
+}
