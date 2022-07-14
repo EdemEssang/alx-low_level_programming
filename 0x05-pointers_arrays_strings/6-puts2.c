6ad1203aa8feafdf4de788d27d90309f3eec3309
Edem <edemoski.eu@gmail.com>
2022-07-13 21:10:59 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * puts2 - is a function that print only one character out of two
+ * starting with the first one
+ * @str: input
+ *
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] !+ '\0'; i++)
+	{
+		if ((i % 2) == 0)
+			putchar(str[i]);
+		else
+			continue;
+	}
+	putchar('\n');
+}
