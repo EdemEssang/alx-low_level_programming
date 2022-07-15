e1940747c7d53a72f66d027def4b7cf3a15e7ae9
Edem <edemoski.eu@gmail.com>
2022-07-15 06:38:03 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * _strcat - a function that concatenates two strings
+ *
+ * @dest: result of string to be modified
+ * @src: string to be appended
+ *
+ * Return: pointer to the resulting string @dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int srclen;
+	int destlen;
+
+
+	while (src[srclen] != '\0')
+		srclen++;
+
+	while (dest[destlen] != '\0')
+
+
+	for (i = 0; i <= srclen; i++)
+	{
+		dest[destlen] = src[i];
+		destlen2++;
+	}
+                   return (dest);
+
+}
