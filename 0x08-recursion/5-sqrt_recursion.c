726d0da652d9bafbb3ec84ce665a73538015110a
Edem Essang <edemoski.eu@gmail.com>
2022-07-22 05:35:35 +0100

    corrections

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,31 @@
 #include<stdio.h>
+
+
+/**
+* power_operation - returns the natural square root of a number.
+* we have to solve for the square root logic first before calling it in the recursion function
+* @n: input number.
+* @c: iterator.
+* Return: square root or -1.
+*/
+int power_operation(int n, int c)
+{
+	if (c % (n / c) == 0)
+	{
+
+		if (c * (n / c) == n)
+
+			return (c);
+
+		else
+
+			return (-1);
+
+	}
+	return (0 + power_operation(n, c + 1));
+
+}
+
 /**
 * _sqrt_recursion - this function returns the natural square root of a number.
 * @n: input number.
