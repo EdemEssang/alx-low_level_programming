cbef07591856c74ff340886b95eda23f7bbafce4
Edem <edemoski.eu@gmail.com>
2022-08-20 22:42:24 -0700

    The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,51 +1,42 @@
+#include <stdio.h>
 /**
- * A program that prints all possible combinations of three different 
- * digits,
- * seperated by a comma followed by a space
- * in ascending order
- * let "x" represent the digits
- */
-
-
+* main - print numbers from 00 to 99.
+*
+* Return: 0 on success
+*/
 int main(void)
 {
+	int i = '0';
+	int j = '0';
+	int k = '0';
 
-    int x1, x2, x3;
+	while (i <= '7')
+	{
+		while (j <= '8')
+		{
+			while (k <= '9')
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 
-     for (x1 = 0; x1 < 8; x1++)
-     {
-      
-     	 for (x2 = x1 + 1; x2 < 9; x2++)
-	 {
-	 
-	    for (x3 = x2 + 1; x3 < 10; x3++)
-	    {
-	    
-	        putchar((x1 % 10) + '0');
-		putchar((x2 % 10) + '0');
-	        putchar((x3 % 10) + '0');
-
-
-		if (x1 == 7 && x2 == 8 && x3 == 9)
-			continue;
-
-
-		putchar(',');
-		putchar(' ');
-	    
-	    
-	    }
-
-
-	 }
-      
-      
+				}
+				k++;
+			}
+			k = '0';
+			j++;
+		}
+		j = '0';
+		i++;
 	}
-
-     putchar('\n');
-
-     return (0);
-
-
-
+	putchar('\n');
+	return (0);
 }
+
