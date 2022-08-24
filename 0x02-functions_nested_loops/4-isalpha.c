763c762fa99b867a7482ec358f0da1a48d6f50fc
Edem <edemoski.eu@gmail.com>
2022-08-23 18:58:24 -0700

    fix trailling whitespaces

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,14 @@
 /**
 *_isalpha - A function that checks for alphabetic character (uppercase 
 * || lowercase)
-*@c - input character
+* @c: takes input from other functions.
 *
 *Return: 1 if c is a letter, lowercase or uppercase
-*Returns 0 otherwise
+*
 */
 int _isalpha(int c)
 {
-	if ((( c>= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
